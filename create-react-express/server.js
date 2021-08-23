a0fd6e1d0cadfd1c87bfd48378e9bbb097b17047
Joey Davidson <j.davidson18@gmail.com>
2021-08-23 11:07:50 -0600

    server complete

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -1,24 +1,25 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
-const app = express();
 
-// Define middleware here
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
+app.use(express.json);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+//add routes, both API and view
+app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+//connect to the MONGO DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/blogReact");
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+//start the API server
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
