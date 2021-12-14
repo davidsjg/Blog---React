7ca00a5b49fb5e5c60b3348f67d7c7ff28ab2189
Joey Davidson <j.davidson18@gmail.com>
2021-12-14 06:41:05 -0700

    initalState successfully loaded into store, console logged successfully

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import store from "./store";
+
+//log the initial state
+console.log("Initial state: ", store.getState());
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
